c013d21f919410d5ceff281202372ec0728c1a36
何旦 <hed@pkusoft.net>
2022-04-20 10:13:01 +0800

    配置文件提交； 执法公开代码提交；

---
+++ src/main/java/com/pkusoft/zfgk/mapper/ZfgkCasePublicMapper.java
@@ -0,0 +1,8 @@
+package com.pkusoft.zfgk.mapper;
+
+import com.pkusoft.zfgk.model.ZfgkCasePublic;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface ZfgkCasePublicMapper extends Mapper<ZfgkCasePublic> {
+
+}
---
+++ src/main/java/com/pkusoft/zfgk/mapper/ZfgkXzfyTemplateMapper.java
@@ -0,0 +1,9 @@
+package com.pkusoft.zfgk.mapper;
+
+import com.pkusoft.zfgk.model.ZfgkXzfyTemplate;
+import com.pkusoft.zfsp.model.RsSypGlxx;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface ZfgkXzfyTemplateMapper extends Mapper<ZfgkXzfyTemplate> {
+
+}
