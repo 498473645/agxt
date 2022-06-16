722177731ee8fe13dae579345bc7dc7a10228045
huangh <498473645@qq.com>
2022-06-16 15:48:47 +0800

    报案办事监督权限接口

--- src/main/java/com/pkusoft/usercenter/service/impl/SysPermitServiceImpl.java
+++ src/main/java/com/pkusoft/usercenter/service/impl/SysPermitServiceImpl.java
@@ -1,7 +1,9 @@
 package com.pkusoft.usercenter.service.impl;
 
 import com.pkusoft.usercenter.mapper.SysPermitMapper;
+import com.pkusoft.usercenter.po.SysDataOwnerDept;
 import com.pkusoft.usercenter.po.SysDept;
+import com.pkusoft.usercenter.service.SysDataOwnerDeptService;
 import com.pkusoft.usercenter.service.SysDeptService;
 import com.pkusoft.usercenter.service.SysPermitService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,11 +11,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
-import pkubatis.common.utils.CodeUtils;
 import pkubatis.common.utils.PermitType;
-import pkubatis.toolkit.SpringContextHolder;
 import tk.mybatis.mapper.entity.Example;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -32,6 +33,9 @@ public class SysPermitServiceImpl implements SysPermitService {
 
     @Autowired
     private SysPermitService sysPermitService;
+
+    @Autowired
+    private SysDataOwnerDeptService sysDataOwnerDeptService;
 
     /**
      * 根据当前用户id和权限类型获取用户角色下的权限信息的值
@@ -212,6 +216,99 @@ public class SysPermitServiceImpl implements SysPermitService {
                 break;
             default:
                 break;
+        }
+    }
+
+    /**
+     * 报案办事监督当前用户角色数据权限
+     * @param criteria    	查询条件
+     * @param sysUser		用户信息
+     * @param permitsType 	权限类型
+     */
+    @Override
+    public void setUserDataPermits_2(Example.Criteria criteria, Map<String, String> sysUser, String permitsType)  {
+        try {
+            //查询当前用户角色数据权限
+            String permitValue = getUserPermitValue(sysUser.get("userId"), permitsType);
+            if(StringUtils.hasText(permitValue)){
+                String deptId = this.getDeptIdBypermit(sysUser,permitValue);
+                switch (permitsType) {
+                    case PermitType.PERMITTYPE_100002:
+                        setAjxxDataPermits(criteria, sysUser, deptId, permitValue);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            throw new RuntimeException("查询当前用户角色数据权限错误");
+        }
+    }
+
+    public String getDeptIdBypermit(Map<String, String> sysUser, String permitValue) throws IOException {
+        String deptId = "";
+        //1.根据登录人及权限等级查询权限部门id,根据用户所属部门查询数据归属单位表
+        SysDataOwnerDept owner = sysDataOwnerDeptService.selectByDeptId(sysUser.get("deptId"));
+        if (owner==null){
+            return deptId;
+        }
+        //循环判断对应权限所属部门
+        switch (permitValue){
+            //省级单位数据查询权限
+            case PermitType.PERMIT_VALUE_DEPT_1:
+                deptId = owner.getOwnerDept1();
+                break;
+            //市局单位数据查询权限
+            case PermitType.PERMIT_VALUE_DEPT_2:
+                deptId = owner.getOwnerDept2();
+                break;
+            //分局单位数据查询权限
+            case PermitType.PERMIT_VALUE_DEPT_3:
+                deptId = owner.getOwnerDept3();
+                break;
+            //派出所数据查询权限
+            case PermitType.PERMIT_VALUE_DEPT_4:
+                deptId = owner.getOwnerDept4();
+                break;
+            //所属办案区数据查询权限及本人数据查询权限
+            case PermitType.PERMIT_VALUE_AREA:
+                deptId = owner.getOwnerDept5();
+                break;
+        }
+        return deptId;
+    }
+
+    public void setAjxxDataPermits(Example.Criteria criteria,Map<String, String> sysUser, String deptId, String permitValue) {
+        //判断是否有权限单位
+        if (StringUtils.hasText(deptId)) {
+            switch (permitValue){
+                //省级单位数据查询权限
+                case PermitType.PERMIT_VALUE_DEPT_1:
+                    criteria.andEqualTo("gaOwnerDept1", deptId);
+                    break;
+                //市局单位数据查询权限
+                case PermitType.PERMIT_VALUE_DEPT_2:
+                    criteria.andEqualTo("gaOwnerDept2", deptId);
+                    break;
+                //分局单位数据查询权限
+                case PermitType.PERMIT_VALUE_DEPT_3:
+                    criteria.andEqualTo("gaOwnerDept3", deptId);
+                    break;
+                //派出所数据查询权限
+                case PermitType.PERMIT_VALUE_DEPT_4:
+                    criteria.andEqualTo("gaOwnerDept4", deptId);
+                    break;
+                //本办案区办理的
+                case PermitType.PERMIT_VALUE_AREA:
+                    criteria.andEqualTo("gaOwnerDept5", deptId);
+                    break;
+                //查询所有数据
+                default:
+                    break;
+            }
+        }else {//如果没有权限则查不出数据
+            criteria.andEqualTo("id","0");
         }
     }
 }
